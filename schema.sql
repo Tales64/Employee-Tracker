0e92f8bb47709cabd5ffdebb41345176eec7a7fe
Darryl Tillman <113705249+Tales64@users.noreply.github.com>
2023-05-01 05:26:04 -0700

    department table added

--- schema.sql
+++ schema.sql
@@ -2,15 +2,10 @@ DROP DATABASE IF EXISTS employeeTracker_db;
 CREATE DATABASE employeeTracker_db;
 USE employeeTracker_db;
 
-
-
-
-
-
-
-
-
-
+CREATE TABLE departments (
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+department_name VARCHAR(255) NOT NULL
+);
 
 -- Create an employee Table
 CREATE TABLE employee (
