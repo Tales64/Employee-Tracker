b5e02c1bfe92aad5b9f949d477fae2ff5420d773
Darryl Tillman <113705249+Tales64@users.noreply.github.com>
2023-05-01 04:53:49 -0700

    employee table added

--- schema.sql
+++ schema.sql
@@ -1,3 +1,21 @@
 DROP DATABASE IF EXISTS employeeTracker_db;
 CREATE DATABASE employeeTracker_db;
 USE employeeTracker_db;
+
+
+
+
+
+
+
+
+
+
+
+-- Create an employee Table
+CREATE TABLE employee (
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
+manager_id INT NOT NULL
+);
